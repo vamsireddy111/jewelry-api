cb22172cb7db9eebdfc545c97fc6804424d77e21
Vamsi Reddy <sathivamsi649@gmail.com>
2026-07-08 19:35:47 +0530

    Added base URL home route

--- jewelry_project/urls.py
+++ jewelry_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api import views   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home),   
+
     path('api/', include('api.urls')),
 ]
