6863745d8dbb0e39e6c532cba6a352b2dd0de4cb
vamsireddy111 <sathivamsi649@gmail.com>
2026-07-16 13:37:04 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -18,19 +18,19 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
-    # 🔴 Validate Name
+    #Validate Name
     def validate_name(self, value):
         if not value.strip():
             raise serializers.ValidationError("Name cannot be empty")
         return value
 
-    # 🔴 Validate Price
+    #Validate Price
     def validate_price(self, value):
         if value <= 0:
             raise serializers.ValidationError("Price must be greater than 0")
         return value
 
-    # 🔴 Validate Rating
+    #Validate Rating
     def validate_rating(self, value):
         if value < 0 or value > 5:
             raise serializers.ValidationError("Rating must be between 0 and 5")
